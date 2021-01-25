8938418e63d0b587382e995ffd5992afd22f8409
ZhaoAnkang <zaksq@qq.com>
2021-01-25 13:32:49 +0800

    最长公共子前缀二分法

--- question/easy/LongestCommonPrefix.java
+++ question/easy/LongestCommonPrefix.java
@@ -77,9 +77,9 @@ public class LongestCommonPrefix {
         if(start == end){
             return strs[start];
         }else{
-            int mid = (start + end) / 2 + start;
+            int mid = (end - start) / 2 + start;
             String lcpLeft = longestCommonPrefix(strs,start,mid);
-            String lcpRight = longestCommonPrefix(strs,mid,end);
+            String lcpRight = longestCommonPrefix(strs,mid + 1,end);
             return commonPrefix(lcpLeft,lcpRight);
         }
     }
@@ -92,6 +92,43 @@ public class LongestCommonPrefix {
             }
         }
         return lcpLeft.substring(0,minLength);
+    }
+
+    /**
+     * 二分查找法
+     */
+    public String solution3(String[] strs){
+        if(strs == null || strs.length == 0){
+            return "";
+        }
+        int minLength = Integer.MAX_VALUE;
+        for(String str : strs){
+            minLength = Math.min(minLength, str.length());
+        }
+        int low = 0, high = minLength;
+        while(low < high){
+            int mid = (high - low + 1) / 2 + low;
+            if (isCommonPrefix(strs,mid)){
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return strs[0].substring(0, low);
+    }
+
+    public boolean isCommonPrefix(String[] strs, int length){
+        String str0 = strs[0].substring(0,length);
+        int count = strs.length;
+        for (int i = 1; i < count; i ++){
+            String str = strs[i];
+            for(int j = 0; j < length; j ++){
+                if(str0.charAt(j) != str.charAt(j)){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
